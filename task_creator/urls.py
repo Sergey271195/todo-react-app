cc105d7ff76785111f209083aa7c5350a9c09223
unknown <mackovckin.sergei@yandex.ru>
2020-09-17 14:36:06 +0300

    fourth

--- task_creator/urls.py
+++ task_creator/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
 from .views import userManagerView, taskManagerView, dailyTaskManagerView, commentManagerView, completeTaskView
+from .utilities import EmployeeManager
+from task_creator.bitrix24 import BitrixIntegrator
+
+bitrix = BitrixIntegrator()
+tskManager = EmployeeManager(bitrix)
+tskManager.check_for_new_emloyee()
+
 
 urlpatterns = [
     path('users/', userManagerView),
