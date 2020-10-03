19ae56b7f33221f0ce0b442e47a41cccc248f743
unknown <mackovckin.sergei@yandex.ru>
2020-10-03 18:58:20 +0300

    rm heroku

--- bitrix_project/settings.py
+++ bitrix_project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,4 +99,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
